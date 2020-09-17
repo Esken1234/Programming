40fe8cb435958cec36641d8f481f0b3bf798a369
Esken1234 <eskender341@gmail.com>
2020-09-17 09:09:05 +0300

    1 commit

---
+++ Practice/02/C++/2. Переменные int и double/2. Переменные int и double/2. Переменные int и double.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    int a1; int b1;
+    double a2; double b2;
+    a1 = 3; b1 = 3.14; a2 = 3; b2 = 3.14;
+    cout << a1 << "\n"; cout << b1 << "\n"; cout << a2 << "\n"; cout << b2;
+}
---
+++ Practice/05/C++/5. Падение объекта/5. Падение объекта/5. Падение объекта.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double x0, v0, t;
+    cout << "x0 = ";
+    cin >> x0;
+    cout << "\n";
+    cout << "v0 = ";
+    cin >> v0;
+    cout << "\n";
+    cout << "t = ";
+    cin >> t;
+    cout << "\n";
+    cout << "x(t) = " << x0 + v0 * t - ((9.8 * t * t) / 2);
+    
+
+
+}
